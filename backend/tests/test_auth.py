5d649ee84f530839b4f18b6436e2bcc4795eb8fc
Charles Burbury <burburycharles@gmail.com>
2026-06-17 13:33:52 -0500

    Fixed error where tests would hit actual DB

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,8 +1,3 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
 def test_register_success(client):
     response = client.post("/auth/register", json = {
         "email": "test@example.com",

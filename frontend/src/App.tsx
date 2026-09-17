95ab4cccfb3bb39819af2e1503610884d89c57cc
Charles Burbury <burburycharles@gmail.com>
2026-09-17 14:30:20 -0500

    Wired up router with public and protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,23 @@
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Login from './Login'
+import Register from './Register'
+import Notes from './Notes'
+import ProtectedRoute from './ProtectedRoute'
+
 
 function App() {
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/" element={<Navigate to="/notes" replace />} />
+        <Route element={<ProtectedRoute/>}>
+          <Route path="/notes" element={<Notes/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
